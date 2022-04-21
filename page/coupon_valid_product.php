3792b0aac4aa17fccb527ea3d76358642126754f
Chen-Wei <p1789632p@gmail.com>
2022-04-21 18:13:48 +0800

    new123

--- page/coupon_valid_product.php
+++ page/coupon_valid_product.php
@@ -1,8 +1,21 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . "/project/project-conn.php");
-$sql = "SELECT * FROM coupon_valid_product ";
+if (isset($_GET["id_type"]) && isset($_GET["id"])) {
+    $id_type = $_GET["id_type"];
+    $id = $_GET["id"];
+    $sql = "SELECT * FROM coupon_valid_product WHERE $id_type=$id";
+    $title = "<h3>本次搜索條件 : $id_type=$id</h3>";
+} else {
+    $sql = "SELECT * FROM coupon_valid_product";
+    $title = "";
+}
+
+
+
 $result = $conn->query($sql);
 $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+
 
 
 //兩個接受的值不同就不衝突.
@@ -13,9 +26,28 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <!-- 清單 -->
-<h2>優惠券適用課程</h2>
+<h2>優惠券適用商品</h2>
 <table class="table">
     <thead>
+        <form action="../page/index.php">
+            <select name="id_type" class="form-select" aria-label="Default select example">
+                <option value="product_id">商品 ID</option>
+                <option value="coupon_id">Coupon ID</option>
+            </select>
+            <div class="col-auto">
+                <input type="number" class="form-control" name="id" value="<?= $id ?>">
+            </div>
+            <input class="d-none" type="text" name="current" value="coupon_valid_product">
+            <div class="col-auto">
+                <button type="submit" class="btn btn-info text-white">篩選</button>
+            </div>
+
+            <div class="col-auto">
+                <a class="btn btn-info text-white" href="http://localhost:8080/project/page/index.php?current=coupon_valid_product">重新篩選</a>
+            </div>
+        </form>
+
+        <?php echo $title ?>
         <tr>
             <th scope="col">id</th>
             <th scope="col">product id</th>
@@ -29,11 +61,11 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
     </thead>
     <tbody>
         <?php foreach ($rows as $row) : ?>
-        <tr>
-            <td><?= $row["id"] ?></td>
-            <td><?= $row["product_id"] ?></td>
-            <td><?= $row["coupon_id"] ?></td>
-            <td><?= $row["coupon_id"] ?></td>
+            <tr>
+                <td><?= $row["id"] ?></td>
+                <td><?= $row["product_id"] ?></td>
+                <td><?= $row["coupon_id"] ?></td>
+
             <?php endforeach; ?>
     </tbody>
 </table>
--- page/index.php
+++ page/index.php
@@ -12,26 +12,27 @@ if (isset($_GET["current"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>後台管理</title>
 </head>
 
 <body>
+
     <?php require_once("../layout/header.php") ?>
     <div class="container-fluid body px-0 ">
         <div class="row ">
             <div class="col-2  px-0">
+
                 <?php require_once("../layout/aside.php") ?>
             </div>
             <div class="col-10 border-start p-5">
+
                 <?php require_once("./$currentPage" . ".php") ?>
             </div>
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 
 </html>
