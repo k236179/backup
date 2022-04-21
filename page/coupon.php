3786f480639afa5b27c7e663e08389cf382e7e7c
Chen-Wei <p1789632p@gmail.com>
2022-04-21 17:18:04 +0800

    新增coupon分頁

--- page/coupon.php
+++ page/coupon.php
@@ -1,8 +1,31 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . "/project/project-conn.php");
+if (!isset($_GET["p"])) {
+    $p = 1;
+} else {
+    $p = $_GET["p"];
+}
+
+
+
+//計算總共有幾筆資料
 $sql = "SELECT * FROM coupon ";
 $result = $conn->query($sql);
+$total = $result->num_rows;
+//與總共要計算幾筆分頁有關係
+
+//計算分頁
+$per_page = 10;
+$start = ($p - 1) * $per_page;
+$sql = "SELECT * FROM coupon  LIMIT $start,$per_page";
+$result = $conn->query($sql);
 $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+$page_count = CEIL($total / $per_page);
+//總共會有幾頁
+
+
+
 
 //兩個接受的值不同就不衝突.
 //假設又要在抓一筆資料的時候
@@ -10,8 +33,15 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 ?>
 <!-- 清單 -->
 <div class="form p-3">
-    <h2>優惠券一覽123</h2>
+    <h2>優惠券一覽</h2>
     <table class="table">
+        <div>
+            <div class="py-2 text-end">
+                第<?= $p ?> 頁 , 共<?= $page_count ?>頁 , 共<?= $total ?> 筆資料
+            </div>
+
+        </div>
+
         <thead>
             <tr>
                 <td>id</td>
@@ -40,7 +70,15 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
                 </tr>
             <?php endforeach; ?>
         </tbody>
+
     </table>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <?php for ($i = 1; $i <= $page_count; $i++) : ?>
+                <li class="page-item <?php if ($i == $p) echo "active" ?>"><a class="page-link" href="index.php?current=coupon&p=<?= $i ?>"><?= $i ?></a></li>
+            <?php endfor ?>
+        </ul>
+    </nav>
 </div>
 
 <?php
