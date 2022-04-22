c6d9b6346e4b887d39d90b991534f2b38cb856e6
Chen-Wei <p1789632p@gmail.com>
2022-04-22 15:25:37 +0800

    0422 0325 新增coupon

--- page/coupon_valid_product.php
+++ page/coupon_valid_product.php
@@ -3,24 +3,32 @@ require($_SERVER['DOCUMENT_ROOT'] . "/project/project-conn.php");
 
 if (isset($_GET["id_type"]) && isset($_GET["id"])) {
 
-    // SELECT * FROM product,coupon,coupon_valid_product 
-    // WHERE coupon.id=coupon_valid_product.coupon_id AND product.id=coupon_valid_product.product_id AND coupon_id=1;
 
     $id_type = $_GET["id_type"];
     $id = $_GET["id"];
 
-    if ("$id_type" == "product_id") {
-        $sql = "SELECT * FROM coupon,coupon_valid_product WHERE coupon.id=coupon_valid_product.coupon_id AND $id_type=$id";
-        $title = "<h3>本次搜索條件 : $id_type=$id</h3>";
-    } else if ("$id_type" == "coupon_id") {
-        $sql = "SELECT * FROM product,coupon_valid_product 
-        WHERE product.id=coupon_valid_product.product_id AND $id_type=$id ";
 
-        $title = "<h3>本次搜索條件 : $id_type=$id</h3>";
-    }
+    $sql = "SELECT coupon_valid_product. * , 
+    product.name AS pro_name ,
+    coupon.name, 
+    product.price,
+    product.inventory ,
+    product.createTime 
+    FROM product,coupon_valid_product,coupon 
+    WHERE coupon.id=coupon_valid_product.coupon_id 
+    AND product.id=coupon_valid_product.product_id AND $id_type=$id ";
 } else {
-    $sql = "SELECT * FROM coupon_valid_product ";
-    $title = "";
+    $id_type = "product_id";
+    $id = 1;
+    $sql = "SELECT coupon_valid_product. * , 
+    product.name AS pro_name ,
+    coupon.name, 
+    product.price,
+    product.inventory ,
+    product.createTime 
+    FROM product,coupon_valid_product,coupon 
+    WHERE coupon.id=coupon_valid_product.coupon_id 
+    AND product.id=coupon_valid_product.product_id AND $id_type=$id ";
 }
 
 
@@ -44,8 +52,14 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
     <thead>
         <form action="../page/index.php">
             <select name="id_type" class="form-select" aria-label="Default select example">
-                <option value="product_id">商品 ID</option>
-                <option value="coupon_id">Coupon ID</option>
+                <option value="product_id" <?php if (isset($_GET["id_type"]) && $_GET["id_type"] == "product_id") {
+                                                echo "selected";
+                                            } ?>>商品 ID</option>
+                <option value="coupon_id" <?php if (isset($_GET["id_type"]) && $_GET["id_type"] == "coupon_id") {
+                                                echo "selected";
+                                            } ?>>Coupon ID</option>
+                <!-- 可以使用迴圈的寫法 -->
+
             </select>
             <div class="col-auto">
                 <input type="number" class="form-control" name="id" value="<?= $id ?>">
@@ -63,27 +77,36 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
             </div>
         </form>
 
-        <?php echo $title ?>
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">coupon id</th>
-            <th scope="col">product id</th>
 
-            <th scope="col"><?php
+
+
+        <div scope="col"><?php
                             $title = "新增適用商品";
                             $formType = "post-couponValidProduct";
-                            require("../components/post-offcanvas.php") ?></th>
-        </tr>
-        </tr>
+                            require("../components/post-offcanvas.php") ?></div>
+
     </thead>
     <tbody>
-        <?php foreach ($rows as $row) : ?>
-            <tr>
-                <td><?= $row["id"] ?></td>
-                <td><?= $row['name'] ?></td>
-                <td><?= $row['product_id'] ?></td>
+        <h3><?= $id_type ?>=<?= $id ?>的商品</h3>
 
+        <div class="row">
+            <?php foreach ($rows as $row) : ?>
+                <div class="col-3 gy-3">
+                    <div class="card ">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $row["pro_name"] ?></h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">製造日期<?= $row["createTime"] ?></li>
+                            <li class="list-group-item">庫存: <?= $row["inventory"] ?></li>
+                            <li class="list-group-item">價格: <?= $row["price"] ?></li>
+                            <li class="list-group-item">Coupon: <?= $row["name"] ?></li>
+                        </ul>
+                    </div>
+                </div>
             <?php endforeach; ?>
+        </div>
+
     </tbody>
 </table>
 
