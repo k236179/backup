e5f01fdfc09cf101329266ad01272cd0d69a110e
Chen-Wei <p1789632p@gmail.com>
2022-04-25 18:53:06 +0800

    123456

--- components/edit-modal.php
+++ components/edit-modal.php
@@ -13,8 +13,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <iframe src="http://localhost:8080/project/components/<?= $edit_type ?>-form.php?id=<?= $row["id"] ?>"
-                    frameborder="0" scrolling="no" style="height: 50vh;" class="w-100"></iframe>
+                <iframe src="http://localhost:8080/project/components/<?= $edit_type ?>-form.php?id=<?= $row["id"] ?>" frameborder="0" scrolling="no" style="height: 50vh;" class="w-100"></iframe>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
